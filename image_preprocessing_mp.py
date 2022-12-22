9b6d3519641fc2fa25fee20067efc50d481ffdca
Psemp <57976914+Psemp@users.noreply.github.com>
2022-12-22 20:12:21 +0100

    A bit of sift, a lot of bugs

---
+++ image_preprocessing_mp.py
@@ -0,0 +1,60 @@
+import os
+import glob
+import concurrent.futures
+import warnings
+
+from PIL import Image
+
+from scripts.utils import additionnal_cleaning, noise_and_blur
+
+image_dir = "./data/Images"
+
+image_paths = glob.glob(pathname=f"{image_dir}/*")
+
+
+def preprocessing(image_path):
+    """
+    Preprocesses the image at the given path by transforming it into a square image and resizing it to 128x128.
+
+    Args:
+    - image_path: path to image for pillow to process
+
+    If the image is not already a square, creates a new image, fills it with black and pastes the
+    original image centered. The resulting image is then resized to 128x128.
+
+    Returns:
+    Nothing
+    Saves the preprocessed image in the "./imgs/" (+ mode) directory.
+    """
+    mode = "gs"
+    outdir = f"./imgs/{mode}"
+    image_name = os.path.basename(image_path)
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        image = Image.open(image_path)
+
+    width, height = image.size
+    if width == height:
+        pass
+    elif width > height:
+        new_image = Image.new(image.mode, (width, width), (0, 0, 0))
+        new_image.paste(image, (0, (width - height) // 2))
+        image = new_image
+    else:
+        new_image = Image.new(image.mode, (height, height), (0, 0, 0))
+        new_image.paste(image, ((height - width) // 2, 0))
+        image = new_image
+
+    image = image.resize((128, 128), resample=Image.Resampling.BICUBIC)
+    image = additionnal_cleaning(product_image=image, mode=mode)
+    if mode == "gs" or mode == "gs_he":
+        image = noise_and_blur(product_image=image)
+        image = image.convert(mode="L")
+    image.save(fp=f"{outdir}/{image_name}")
+
+
+workers = os.cpu_count()
+
+if __name__ == "__main__":
+    with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
+        executor.map(preprocessing, image_paths)
